4b9cc0df4a693683721bf1d9673941be9f59e561
Daniel Rodrigues da Silva <tiodanas@gmail.com>
2017-03-16 02:15:26 -0300

    Primeiro commit

---
+++ src/main/java/br/gov/sc/ciasc/weblic/cadpass/CadpassApplication.java
@@ -0,0 +1,12 @@
+package br.gov.sc.ciasc.weblic.cadpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpassApplication.class, args);
+	}
+}
