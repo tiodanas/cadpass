3435ce71928ec3e74ab0e5e5dece85b40467915c
DANIEL RODRIGUES DA SILVA <danielsilva@ciasc.sc.gov.br>
2017-03-24 19:45:31 -0300

    Implementações de views Thymeleaf e métodos de Controllers.

--- src/main/java/br/gov/sc/ciasc/weblic/cadpass/controller/SolicitacaoController.java
+++ src/main/java/br/gov/sc/ciasc/weblic/cadpass/controller/SolicitacaoController.java
@@ -1,13 +1,25 @@
 package br.gov.sc.ciasc.weblic.cadpass.controller;
 
+import br.gov.sc.ciasc.weblic.cadpass.domain.Solicitacao;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SolicitacaoController {
 
     @RequestMapping("/solicitacao/create")
-    String create() {
+    String create(Model model) {
+        Solicitacao solicitacao = new Solicitacao();
+        System.out.println("--- SolicitacaoController.create, solicitacao: " + solicitacao);
+        model.addAttribute("solicitacao", solicitacao);
         return "/solicitacao/create";
     }
+
+    @RequestMapping("/solicitacao/confirm")
+    String confirm(Solicitacao solicitacao, Model model) {
+        model.addAttribute("solicitacao", solicitacao);
+        return "/solicitacao/confirm";
+    }
+
 }
